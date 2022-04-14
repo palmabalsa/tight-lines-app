0b7252e2a76b22a9b4027e0329465d174e6d0e03
Alessandro <73858113+TT-zero@users.noreply.github.com>
2022-04-15 11:08:19 +1200

    working wind api info on forecast cards

--- lib/services/one_call_weather.dart
+++ lib/services/one_call_weather.dart
@@ -11,8 +11,9 @@ import 'package:flutter/material.dart';
    WeatherDataList ramon = WeatherDataList.fromJson(jsonDecode(response.body));
 
      if (response.statusCode == 200) {
-        print (ramon.weatherList?[0].sunrise);
-        print (ramon.weatherList?[3].sunrise);
+        // print (ramon.weatherList?[0].sunrise);
+        // print (ramon.weatherList?[3].sunrise);
+        print(ramon.weatherList?[0]);
         return ramon;
   } else {
     print (response.statusCode);
@@ -20,21 +21,7 @@ import 'package:flutter/material.dart';
   }
 } 
 
-    
-//   WeatherDataList dataHere = WeatherDataList.fromJson(jsonDecode(response.body));
-  
-//   print(dataHere.runtimeType);
-//   print(dataHere.weatherList[1].sunrise);
 
-//   return dataHere;
-
-//  }
-
-
-
-
-  
- 
 
 class WeatherDataList {
   List<WeatherData>? weatherList;
@@ -44,37 +31,29 @@ class WeatherDataList {
   });
   
   factory WeatherDataList.fromJson(Map<String, dynamic> json) {
-      final recievedData = json['daily'] as List<dynamic>?;
-    // List<WeatherData> weatherAnswer = recievedData.map((item) => WeatherData.fromJson(item)).toList();
-    final weatherAnswer = recievedData != null ? recievedData.map((item) => WeatherData.fromJson(item)).toList() : <WeatherData> [];
+      final list = json['daily'] as List<dynamic>?;
+    final weatherAnswer = list != null ? list.map((item) => WeatherData.fromJson(item)).toList() : <WeatherData> [];
 
     return WeatherDataList(
       weatherList: weatherAnswer,
     );
-    }
   }
-
-
-
-    // final recievedData = json['daily'] as List<dynamic>?;
-    // final  weatherList = recievedData != null ? recievedData.map((item) => WeatherData.fromJson(item)).toList() : <WeatherData> [];
-
-    // return WeatherDataList(
-    //   weatherList: weatherList,
-   
-    
-    
+}
     
 
 class WeatherData {
   int dt;
   int sunrise;
   int sunset;
+  double wind_speed;
+  int wind_deg;
  
   WeatherData({
     required this.dt,
     required this.sunrise,
     required this.sunset,
+    required this.wind_speed,
+    required this.wind_deg,
     });
 
 factory WeatherData.fromJson(Map<String, dynamic> json) {
@@ -82,6 +61,8 @@ factory WeatherData.fromJson(Map<String, dynamic> json) {
         dt : json['dt'] as int,
         sunrise: json['sunrise'] as int,
         sunset: json['sunset'] as int,
+        wind_speed: json['wind_speed'] as double,
+        wind_deg: json['wind_deg'] as int,
     );
 }
   
@@ -89,49 +70,46 @@ factory WeatherData.fromJson(Map<String, dynamic> json) {
         'dt': dt,
         'sunrise': sunrise,
         'sunset': sunset,
+        'wind_speed': wind_speed,
+        'wind_deg': wind_deg,
       };
 }
 
 
 
+// class Temp {
+//   int temp;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class ForecastData {
-//   List<WeatherData> forecastList;
-
-//   ForecastData({
-//     required this.forecastList,
+//   Temp ({
+//     required this.temp,
 //   });
 
-//   factory ForecastData.fromJson(Map<String, dynamic> json) => ForecastData(
-//     forecastList: List<WeatherData>.from(json["forecastList"].map((x) => WeatherData.fromJson(x))));
+//   factory Temp.fromJson(Map<String, dynamic> json){
+//     return Temp(
+//       temp: json['day'] as int,
+//     );
+//   }
+// }
 
-//   Map<String, dynamic> toJson() => {
-//     "forecastList" : List<dynamic>.from(forecastList.map((x) => x.toJson())),
-//   };
+
+
+// class WeatherMain {
+//   int id;
+//   String description;
+//   String icon;
+
+//   WeatherMain({
+//     required this.id,
+//     required this.description,
+//     required this.icon,
+//   });
+//   factory WeatherMain.fromJson(Map<String, dynamic> json){
+//     return WeatherMain(
+//       id : json['id'] as int,
+//       description: json ['description'] as String,
+//       icon : json['icon'] as String,
+//       );
+//   }
 // }
 
 
@@ -149,27 +127,72 @@ factory WeatherData.fromJson(Map<String, dynamic> json) {
 
 
 
-// doglas example
-// class ForecastData {
-//   List forecastList;
 
-//   ForecastData({
-//     required this.forecastList,
-//   });
+// import 'package:http/http.dart' as http;
+// import 'dart:convert';
+// import 'dart:async';
+// import 'package:flutter/material.dart';
 
-//   factory ForecastData.fromJson(Map<String, dynamic> json){
-//     List forecastList = [];
 
-//     for (dynamic element in json['forecastList']) {
+//  Future<WeatherDataList> getWeatherData() async {
+//   final String apiUrl = '***REMOVED***data/2.5/onecall?lat=-39.0982&lon=175.8302&exclude=current,minutely,hourly,alerts&units=metric&appid=***REMOVED***';
+//   var response = await http.get(Uri.parse(apiUrl));
 
-//       WeatherData currentForecast = WeatherData(
-//         sunrise: element['daily'][0]['sunrise'],
-//         sunset : element['daily'][0]['sunset']
-//       );
-//       forecastList.add(currentForecast);
-//     }
-//     return ForecastData(
-//       forecastList: forecastList,
-//     );
+//    WeatherDataList ramon = WeatherDataList.fromJson(jsonDecode(response.body));
+
+//      if (response.statusCode == 200) {
+//         // print (ramon.weatherList?[0].sunrise);
+//         // print (ramon.weatherList?[3].sunrise);
+//         print(ramon.weatherList?[0]);
+//         return ramon;
+//   } else {
+//     print (response.statusCode);
+//     throw (Exception('error HERE'));
 //   }
+// } 
+
+
+
+// class WeatherDataList {
+//   List<WeatherData>? weatherList;
+
+//   WeatherDataList ({
+//     required this.weatherList,
+//   });
+  
+//   factory WeatherDataList.fromJson(Map<String, dynamic> json) {
+//       final list = json['daily'] as List<dynamic>?;
+//     final weatherAnswer = list != null ? list.map((item) => WeatherData.fromJson(item)).toList() : <WeatherData> [];
+
+//     return WeatherDataList(
+//       weatherList: weatherAnswer,
+//     );
+//     }
+//   }
+    
+
+// class WeatherData {
+//   int dt;
+//   int sunrise;
+//   int sunset;
+ 
+//   WeatherData({
+//     required this.dt,
+//     required this.sunrise,
+//     required this.sunset,
+//     });
+
+// factory WeatherData.fromJson(Map<String, dynamic> json) {
+//   return WeatherData(
+//         dt : json['dt'] as int,
+//         sunrise: json['sunrise'] as int,
+//         sunset: json['sunset'] as int,
+//     );
+// }
+  
+//   Map<String, dynamic> toJson() => {
+//         'dt': dt,
+//         'sunrise': sunrise,
+//         'sunset': sunset,
+//       };
 // }
--- lib/views/weather_forecast.dart
+++ lib/views/weather_forecast.dart
@@ -27,63 +27,68 @@ late Future<WeatherDataList> aloha;
           title: Text('Todays Weather'),
         ),
         body: Center(
+          child: Column(
+            children: [
+              Text(' Heres todays weather'),
+              FutureBuilder<WeatherDataList>(
+                future: aloha,
+                builder: (context, snapshot) {
 
+                  if (snapshot.hasData) {
 
-          // child: Text('hello'),
-          child: FutureBuilder<WeatherDataList>(
-            future: aloha,
-            builder: (context, snapshot) {
-              // if (snapshot.connectionState == ConnectionState.done) {
-               if (snapshot.hasData){
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                      height: 300.0,
+                      child: ListView.builder(
+                        itemCount: snapshot.data!.weatherList?.length, 
+                        scrollDirection: Axis.horizontal, 
+                        itemBuilder: (context, index) {
+                          return 
+                          SizedBox(
+                            height: 200.0,
+                            width: 150.0,
+                            child: Card(
+                              child: Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Text('${snapshot.data!.weatherList?[index].dt}'.toString()),
+                                    Text('${snapshot.data!.weatherList?[index].sunrise}'.toString()),
+                                    Text('${snapshot.data!.weatherList?[index].sunset}'.toString()),
+                                    Text('WIND'),
+                                    Text('${snapshot.data!.weatherList?[index].wind_speed}'.toString()),
+                                    Text('${snapshot.data!.weatherList?[index].wind_deg}'.toString()),
 
-                 var hello = snapshot.data!.weatherList?[0].sunrise;
-                 var bye = snapshot.data!.weatherList?[0].sunset;
-                
-                print(hello);
-                
-                return Text('$hello + $bye');
-                // return Text('${snapshot.data!.weatherList[1].sunrise}');
-              }
-              else if (snapshot.hasError){
-                throw Exception('theres an error mate');
-              }
-              else return CircularProgressIndicator();
-            }
+                                  // Text(snapshot.data!.description),
+                                  // Text (snapshot.data!.icon),
+                                  // Text('WIND'),
+                                  // Text ('Speed: ${snapshot.data!.speed} Kms'),
+                                  // Text ('Dir: ${snapshot.data!.deg} C'),
+                                  ]
+                                )
+                              )
+                            )
+                          );
+                        }
+                        // => ListTile(
+                        //   title: Text('${snapshot.data!.weatherList?[index].sunrise}'.toString()),
+                        //   subtitle: Text('${snapshot.data!.weatherList?[index].sunset}'.toString()),
+                        // )
+                      )
+                    )
+                    
+              );
+                  }
+                  else if (snapshot.hasError){
+                  throw Exception('theres an error mate');
+                  }
+                    return CircularProgressIndicator();
+                } 
+              )
+           ],
           )
-
-
-
-
-
-
-
-
-
-          // child: FutureBuilder<WeatherDataList>(
-          //   future: aloha,
-          //   builder: (context, snapshot) {
-
-          //     if (snapshot.hasData) {
-
-          //       dynamic maybe = snapshot.data!.theForecast;
-          //       print( maybe);
-
-
-          //         // return ListView.builder(
-          //         //   itemCount: snapshot.data!.length, 
-          //         //   scrollDirection: Axis.horizontal, 
-          //         //   itemBuilder: (context, index) => ListTile(
-          //         //     title: Text(snapshot.data![index].sunrise.toString()),
-          //         //     subtitle: Text(snapshot.data![index].sunset.toString()),
-          //         //   )
-          //         // );       
-          //     }
-          //     else if (snapshot.hasError){
-          //     throw Exception('theres an error');
-          //     }
-          //     return CircularProgressIndicator();
-          //   } 
-          // )
         )
       );
     }
