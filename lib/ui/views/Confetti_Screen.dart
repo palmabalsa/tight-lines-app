31e8fc61eb269dab40843ff587a0e3caa466e003
Alessandro <73858113+TT-zero@users.noreply.github.com>
2022-09-09 08:28:20 +1200

    add confetti success screen

--- lib/ui/views/Confetti_Screen.dart
+++ lib/ui/views/Confetti_Screen.dart
@@ -1,65 +1,101 @@
-// // ignore_for_file: must_be_immutable
+// ignore_for_file: must_be_immutable
+import 'dart:math';
+import 'package:confetti/confetti.dart';
+import 'package:flutter/material.dart';
 
-// import 'dart:math';
-// import 'package:confetti/confetti.dart';
-// import 'package:flutter/material.dart';
+class ConfettiPlayer extends StatefulWidget {
+  ConfettiPlayer({
+    Key? key,
+    this.fishSpecies,
+  }) : super(key: key);
 
-// class ConfettiPlayer extends StatefulWidget {
-//   ConfettiPlayer({
-//     Key? key,
-//     required this.confettiPlaying,
-//   }) : super(key: key);
+  String? fishSpecies;
 
-//   bool confettiPlaying;
+  @override
+  State<ConfettiPlayer> createState() => _ConfettiPlayerState();
+}
 
-//   @override
-//   State<ConfettiPlayer> createState() => _ConfettiPlayerState();
-// }
+class _ConfettiPlayerState extends State<ConfettiPlayer> {
+  late ConfettiController _confettiController;
 
-// class _ConfettiPlayerState extends State<ConfettiPlayer> {
-//   bool ConfettiIsPlaying = false;
-//   final confettiControl = ConfettiController();
+  @override
+  void initState() {
+    super.initState();
+    _confettiController =
+        ConfettiController(duration: const Duration(seconds: 10));
+    _confettiController.play();
+  }
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     confettiControl.play();
-//   }
+  @override
+  void dispose() {
+    _confettiController.dispose();
+    super.dispose();
+  }
 
-//   @override
-//   void dispose() {
-//     confettiControl.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) =>
-
-//       Stack(alignment: Alignment.topCenter, children: [
-//         Scaffold(
-//             backgroundColor: Colors.black,
-//             body: Center(
-//                 child: ListView(children: [
-//               ConfettiWidget(
-//                 confettiController: confettiControl,
-//                 numberOfParticles: 20,
-//                 shouldLoop: false,
-//                 emissionFrequency: 0.00,
-//                 blastDirection: -pi / 2,
-//                 minBlastForce: 5,
-//                 gravity: 0.2,
-//               ),
-//               confettiControl.play();
-//               Padding(
-//                   padding: EdgeInsets.all(100),
-//                   child: SizedBox(
-//                       height: 50, width: 100, child: Text('Nice Catch!'))),
-//               GestureDetector(
-//                 onTap: () {
-//                   // confettiControl.stop();
-//                   Navigator.pushReplacementNamed(context, '/fishinglog');
-//                 },
-//               )
-//             ])))
-//       ]);
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.teal.shade100,
+        body: Stack(alignment: Alignment.topCenter, children: <Widget>[
+          GestureDetector(
+            onTap: () {
+              Navigator.pushReplacementNamed(context, '/fishinglog');
+            },
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            // alignment: Alignment.center,
+            child: ConfettiWidget(
+              confettiController: _confettiController,
+              numberOfParticles: 10,
+              shouldLoop: false,
+              emissionFrequency: 0.1,
+              colors: [
+                Colors.greenAccent.shade700,
+                Colors.blueAccent,
+                Colors.orangeAccent,
+                Colors.yellowAccent,
+                Colors.redAccent,
+                Colors.purpleAccent,
+              ],
+              // blastDirectionality: BlastDirectionality.explosive,
+              blastDirection: -pi / 2,
+              maxBlastForce: 8,
+              minBlastForce: 2,
+              gravity: 0.2,
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: SizedBox(
+                height: 400,
+                width: 300,
+                child: Image.asset('assets/images/jumping_trout.png')),
+          ),
+          Align(
+              alignment: Alignment(-0.01, -0.12),
+              child: SizedBox(
+                  width: 200,
+                  child: TextButton(
+                      style: ButtonStyle(
+                          fixedSize: MaterialStateProperty.resolveWith(
+                              (states) => Size.fromWidth(120)),
+                          backgroundColor: MaterialStateProperty.resolveWith(
+                              (states) => Colors.black)),
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(context, '/fishinglog');
+                      },
+                      child: widget.fishSpecies != null
+                          ? Text('Nice ${widget.fishSpecies}!',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold))
+                          : Text('Nice Fish!',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold))))),
+        ]));
+  }
+}
