4c9d0b81714bd7438636b77b190ef9569ee612ab
Alessandro <73858113+TT-zero@users.noreply.github.com>
2022-09-09 09:35:22 +1200

    jumping trout animation

---
+++ lib/ui/widgets/animated_trout.dart
@@ -0,0 +1,18 @@
+
+//  import 'dart:html';
+
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/src/foundation/key.dart';
+// import 'package:flutter/src/widgets/framework.dart';
+
+// // Image.asset('assets/images/trout.png', color: Colors.white),
+
+
+
+// class animatedTrout extends AnimatedWidget {
+//   @override
+//   Widget build(BuildContext context){
+//     return Image.asset('assets/images/trout.png', color: Colors.black);
+//   }
+// }
